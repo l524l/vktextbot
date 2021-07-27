6014a5bc338935e4c84a926f78874724afaf19f8
l524l <65085630+l524l@users.noreply.github.com>
2021-07-27 18:03:34 +0300

    Initial commit

---
+++ src/main/java/com/l524l/vktextbot/VkTextBotApplication.java
@@ -0,0 +1,13 @@
+package com.l524l.vktextbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkTextBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkTextBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/l524l/vktextbot/VkTextBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l524l.vktextbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkTextBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
