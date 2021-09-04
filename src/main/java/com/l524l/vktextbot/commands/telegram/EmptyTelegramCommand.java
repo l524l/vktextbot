3d0d0bf1cc0042dbc89bae70adfab899624bba93
l524l <65085630+l524l@users.noreply.github.com>
2021-09-04 15:22:09 +0300

    Пустые команды теперь более пустые

--- src/main/java/com/l524l/vktextbot/commands/telegram/EmptyTelegramCommand.java
+++ src/main/java/com/l524l/vktextbot/commands/telegram/EmptyTelegramCommand.java
@@ -1,12 +1,9 @@
 package com.l524l.vktextbot.commands.telegram;
 
-import com.l524l.vktextbot.user.User;
-import org.telegram.telegrambots.meta.api.objects.Update;
-
 public class EmptyTelegramCommand extends TelegramCommand {
 
-    public EmptyTelegramCommand(User executor, Update update) {
-        super(executor, update);
+    public EmptyTelegramCommand() {
+        super(null, null, null);
     }
 
     @Override
--- src/main/java/com/l524l/vktextbot/commands/vk/EmptyVkCommand.java
+++ src/main/java/com/l524l/vktextbot/commands/vk/EmptyVkCommand.java
@@ -1,12 +1,9 @@
 package com.l524l.vktextbot.commands.vk;
 
-import com.l524l.vktextbot.user.User;
-import com.vk.api.sdk.objects.callback.messages.CallbackMessage;
-
 public class EmptyVkCommand extends VkCommand {
 
-    public EmptyVkCommand(User executor, CallbackMessage<?> callbackMessage) {
-        super(executor, callbackMessage);
+    public EmptyVkCommand() {
+        super(null, null, null);
     }
 
     @Override
