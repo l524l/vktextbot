93a321473e79e6c20995a84b9afcda15d548af05
l524l <65085630+l524l@users.noreply.github.com>
2021-09-12 13:08:30 +0300

    Доработан CommandType

--- src/main/java/com/l524l/vktextbot/commands/CommandType.java
+++ src/main/java/com/l524l/vktextbot/commands/CommandType.java
@@ -1,5 +1,24 @@
 package com.l524l.vktextbot.commands;
 
 public enum CommandType {
-    EMPTY_COMMAND, HELLO_COMMAND
+    EMPTY_COMMAND("empty"),
+    HELLO_COMMAND("hello");
+
+    private String commandName;
+
+    CommandType(String commandName) {
+        this.commandName = commandName;
+    }
+
+    public String getCommandName() {
+        return commandName;
+    }
+
+    public CommandType getByCommandName(String commandName) {
+        for (CommandType type:
+             CommandType.values()) {
+            if (type.getCommandName().equals(commandName)) return type;
+        }
+        return EMPTY_COMMAND;
+    }
 }
